d8542334272f812c6537d2742c1826acf6b4a792
Principe Alejando Vásquez Tenazoa <pava@cmacmaynas.com.pe>
2022-03-30 19:46:26 -0500

    Comiteo del proyecto SISOL el formulario 1

---
+++ javascript/javascript.js
@@ -0,0 +1,532 @@
+var clic = 1;
+function divLogin(){ 
+   if(clic==1){
+    document.getElementById("caja").style.paddingTop = "10px";
+    document.getElementById("caja").style.height = "45px";
+   clic = clic + 1;
+   } else{
+        document.getElementById("caja").style.height = "0px";
+        document.getElementById("caja").style.paddingTop = "0px"; 
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function divLogin2(){ 
+   if(clic==1){
+    document.getElementById("caja2").style.paddingTop = "10px";
+    document.getElementById("caja2").style.height = "45px";
+   clic = clic + 1;
+   } else{
+        document.getElementById("caja2").style.height = "0px";
+        document.getElementById("caja2").style.paddingTop = "0px"; 
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function divLogin3(){ 
+   if(clic==1){
+    document.getElementById("caja3").style.paddingTop = "10px";
+    document.getElementById("caja3").style.height = "45px";
+   clic = clic + 1;
+   } else{
+        document.getElementById("caja3").style.height = "0px";
+        document.getElementById("caja3").style.paddingTop = "0px"; 
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function divLogin4(){ 
+   if(clic==1){
+    document.getElementById("caja4").style.paddingTop = "10px";
+    document.getElementById("caja4").style.height = "45px";
+   clic = clic + 1;
+   } else{
+        document.getElementById("caja4").style.height = "0px";
+        document.getElementById("caja4").style.paddingTop = "0px"; 
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function divLogin5(){ 
+   if(clic==1){
+    document.getElementById("caja5").style.paddingTop = "10px";
+    document.getElementById("caja5").style.height = "45px";
+   clic = clic + 1;
+   } else{
+        document.getElementById("caja5").style.height = "0px";
+        document.getElementById("caja5").style.paddingTop = "0px"; 
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function divLogin6(){ 
+   if(clic==1){
+    document.getElementById("caja6").style.paddingTop = "10px";
+    document.getElementById("caja6").style.height = "45px";
+   clic = clic + 1;
+   } else{
+        document.getElementById("caja6").style.height = "0px";
+        document.getElementById("caja6").style.paddingTop = "0px"; 
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function divLogin7(){ 
+   if(clic==1){
+    document.getElementById("caja7").style.paddingTop = "10px";
+    document.getElementById("caja7").style.height = "45px";
+   clic = clic + 1;
+   } else{
+        document.getElementById("caja7").style.height = "0px";
+        document.getElementById("caja7").style.paddingTop = "0px"; 
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function divLogin8(){ 
+   if(clic==1){
+    document.getElementById("caja8").style.paddingTop = "10px";
+    document.getElementById("caja8").style.height = "45px";
+   clic = clic + 1;
+   } else{
+        document.getElementById("caja8").style.height = "0px";
+        document.getElementById("caja8").style.paddingTop = "0px"; 
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function divLogin9(){ 
+   if(clic==1){
+    document.getElementById("caja9").style.paddingTop = "10px";
+    document.getElementById("caja9").style.height = "45px";
+   clic = clic + 1;
+   } else{
+        document.getElementById("caja9").style.height = "0px";
+        document.getElementById("caja9").style.paddingTop = "0px"; 
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function divLogin10(){ 
+   if(clic==1){
+    document.getElementById("caja10").style.paddingTop = "10px";
+    document.getElementById("caja10").style.height = "45px";
+   clic = clic + 1;
+   } else{
+        document.getElementById("caja10").style.height = "0px";
+        document.getElementById("caja10").style.paddingTop = "0px"; 
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function divLogin11(){ 
+   if(clic==1){
+    document.getElementById("caja11").style.paddingTop = "10px";
+    document.getElementById("caja11").style.height = "45px";
+   clic = clic + 1;
+   } else{
+        document.getElementById("caja11").style.height = "0px";
+        document.getElementById("caja11").style.paddingTop = "0px"; 
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function divLogin12(){ 
+   if(clic==1){
+    document.getElementById("caja12").style.paddingTop = "10px";
+    document.getElementById("caja12").style.height = "45px";
+   clic = clic + 1;
+   } else{
+        document.getElementById("caja12").style.height = "0px";
+        document.getElementById("caja12").style.paddingTop = "0px"; 
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function divLogin13(){ 
+   if(clic==1){
+    document.getElementById("caja13").style.paddingTop = "10px";
+    document.getElementById("caja13").style.height = "45px";
+   clic = clic + 1;
+   } else{
+        document.getElementById("caja13").style.height = "0px";
+        document.getElementById("caja13").style.paddingTop = "0px"; 
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function divLogin14(){ 
+   if(clic==1){
+    document.getElementById("caja14").style.paddingTop = "10px";
+    document.getElementById("caja14").style.height = "45px";
+   clic = clic + 1;
+   } else{
+        document.getElementById("caja14").style.height = "0px";
+        document.getElementById("caja14").style.paddingTop = "0px"; 
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function divLogin15(){ 
+   if(clic==1){
+    document.getElementById("caja15").style.paddingTop = "10px";
+    document.getElementById("caja15").style.height = "45px";
+   clic = clic + 1;
+   } else{
+        document.getElementById("caja15").style.height = "0px";
+        document.getElementById("caja15").style.paddingTop = "0px"; 
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function divLogin16(){ 
+   if(clic==1){
+    document.getElementById("caja16").style.paddingTop = "10px";
+    document.getElementById("caja16").style.height = "45px";
+   clic = clic + 1;
+   } else{
+        document.getElementById("caja16").style.height = "0px";
+        document.getElementById("caja16").style.paddingTop = "0px"; 
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function divLogin17(){ 
+   if(clic==1){
+    document.getElementById("caja17").style.paddingTop = "10px";
+    document.getElementById("caja17").style.height = "45px";
+   clic = clic + 1;
+   } else{
+        document.getElementById("caja17").style.height = "0px";
+        document.getElementById("caja17").style.paddingTop = "0px"; 
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function divLogin18(){ 
+   if(clic==1){
+    document.getElementById("caja18").style.paddingTop = "10px";
+    document.getElementById("caja18").style.height = "45px";
+   clic = clic + 1;
+   } else{
+        document.getElementById("caja18").style.height = "0px";
+        document.getElementById("caja18").style.paddingTop = "0px"; 
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function divLogin19(){ 
+   if(clic==1){
+    document.getElementById("caja19").style.paddingTop = "10px";
+    document.getElementById("caja19").style.height = "45px";
+   clic = clic + 1;
+   } else{
+        document.getElementById("caja19").style.height = "0px";
+        document.getElementById("caja19").style.paddingTop = "0px"; 
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function divLogin20(){ 
+   if(clic==1){
+    document.getElementById("caja20").style.paddingTop = "10px";
+    document.getElementById("caja20").style.height = "45px";
+   clic = clic + 1;
+   } else{
+        document.getElementById("caja20").style.height = "0px";
+        document.getElementById("caja20").style.paddingTop = "0px"; 
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function divLogin21(){ 
+   if(clic==1){
+    document.getElementById("caja21").style.paddingTop = "10px";
+    document.getElementById("caja21").style.height = "45px";
+   clic = clic + 1;
+   } else{
+        document.getElementById("caja21").style.height = "0px";
+        document.getElementById("caja21").style.paddingTop = "0px"; 
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function divLogin22(){ 
+   if(clic==1){
+    document.getElementById("caja22").style.paddingTop = "10px";
+    document.getElementById("caja22").style.height = "45px";
+   clic = clic + 1;
+   } else{
+        document.getElementById("caja22").style.height = "0px";
+        document.getElementById("caja22").style.paddingTop = "0px"; 
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function divLogin23(){ 
+   if(clic==1){
+    document.getElementById("caja23").style.paddingTop = "10px";
+    document.getElementById("caja23").style.height = "45px";
+   clic = clic + 1;
+   } else{
+        document.getElementById("caja23").style.height = "0px";
+        document.getElementById("caja23").style.paddingTop = "0px"; 
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function divLogin24(){ 
+   if(clic==1){
+    document.getElementById("caja24").style.paddingTop = "10px";
+    document.getElementById("caja24").style.height = "45px";
+   clic = clic + 1;
+   } else{
+        document.getElementById("caja24").style.height = "0px";
+        document.getElementById("caja24").style.paddingTop = "0px"; 
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function divLogin25(){ 
+   if(clic==1){
+    document.getElementById("caja25").style.paddingTop = "10px";
+    document.getElementById("caja25").style.height = "45px";
+   clic = clic + 1;
+   } else{
+        document.getElementById("caja25").style.height = "0px";
+        document.getElementById("caja25").style.paddingTop = "0px"; 
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function divLogin26(){ 
+   if(clic==1){
+    document.getElementById("caja26").style.paddingTop = "10px";
+    document.getElementById("caja26").style.height = "45px";
+   clic = clic + 1;
+   } else{
+        document.getElementById("caja26").style.height = "0px";
+        document.getElementById("caja26").style.paddingTop = "0px"; 
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function divLogin27(){ 
+   if(clic==1){
+    document.getElementById("caja27").style.paddingTop = "10px";
+    document.getElementById("caja27").style.height = "45px";
+   clic = clic + 1;
+   } else{
+        document.getElementById("caja27").style.height = "0px";
+        document.getElementById("caja27").style.paddingTop = "0px"; 
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function divLogin28(){ 
+   if(clic==1){
+    document.getElementById("caja28").style.paddingTop = "10px";
+    document.getElementById("caja28").style.height = "45px";
+   clic = clic + 1;
+   } else{
+        document.getElementById("caja28").style.height = "0px";
+        document.getElementById("caja28").style.paddingTop = "0px"; 
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function divLogin29(){ 
+   if(clic==1){
+    document.getElementById("caja29").style.paddingTop = "10px";
+    document.getElementById("caja29").style.height = "45px";
+   clic = clic + 1;
+   } else{
+        document.getElementById("caja29").style.height = "0px";
+        document.getElementById("caja29").style.paddingTop = "0px"; 
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function divLogin30(){ 
+   if(clic==1){
+    document.getElementById("caja30").style.paddingTop = "10px";
+    document.getElementById("caja30").style.height = "45px";
+   clic = clic + 1;
+   } else{
+        document.getElementById("caja30").style.height = "0px";
+        document.getElementById("caja30").style.paddingTop = "0px"; 
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function divLogin31(){ 
+   if(clic==1){
+    document.getElementById("caja31").style.paddingTop = "10px";
+    document.getElementById("caja31").style.height = "45px";
+   clic = clic + 1;
+   } else{
+        document.getElementById("caja31").style.height = "0px";
+        document.getElementById("caja31").style.paddingTop = "0px"; 
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function divLogin32(){ 
+   if(clic==1){
+    document.getElementById("caja32").style.paddingTop = "10px";
+    document.getElementById("caja32").style.height = "45px";
+   clic = clic + 1;
+   } else{
+        document.getElementById("caja32").style.height = "0px";
+        document.getElementById("caja32").style.paddingTop = "0px"; 
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function divLogin33(){ 
+   if(clic==1){
+    document.getElementById("caja33").style.paddingTop = "10px";
+    document.getElementById("caja33").style.height = "45px";
+   clic = clic + 1;
+   } else{
+        document.getElementById("caja33").style.height = "0px";
+        document.getElementById("caja33").style.paddingTop = "0px"; 
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function divLogin34(){ 
+   if(clic==1){
+    document.getElementById("caja34").style.paddingTop = "10px";
+    document.getElementById("caja34").style.height = "45px";
+   clic = clic + 1;
+   } else{
+        document.getElementById("caja34").style.height = "0px";
+        document.getElementById("caja34").style.paddingTop = "0px"; 
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function divLogin35(){ 
+   if(clic==1){
+    document.getElementById("caja35").style.paddingTop = "10px";
+    document.getElementById("caja35").style.height = "45px";
+   clic = clic + 1;
+   } else{
+        document.getElementById("caja35").style.height = "0px";
+        document.getElementById("caja35").style.paddingTop = "0px"; 
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function divLogin36(){ 
+   if(clic==1){
+    document.getElementById("caja36").style.paddingTop = "10px";
+    document.getElementById("caja36").style.height = "45px";
+   clic = clic + 1;
+   } else{
+        document.getElementById("caja36").style.height = "0px";
+        document.getElementById("caja36").style.paddingTop = "0px"; 
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function divLogin37(){ 
+   if(clic==1){
+    document.getElementById("caja37").style.paddingTop = "10px";
+    document.getElementById("caja37").style.height = "45px";
+   clic = clic + 1;
+   } else{
+        document.getElementById("caja37").style.height = "0px";
+        document.getElementById("caja37").style.paddingTop = "0px"; 
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function divLogin38(){ 
+   if(clic==1){
+    document.getElementById("caja38").style.paddingTop = "10px";
+    document.getElementById("caja38").style.height = "45px";
+   clic = clic + 1;
+   } else{
+        document.getElementById("caja38").style.height = "0px";
+        document.getElementById("caja38").style.paddingTop = "0px"; 
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function divLogin39(){ 
+   if(clic==1){
+    document.getElementById("caja39").style.paddingTop = "10px";
+    document.getElementById("caja39").style.height = "45px";
+   clic = clic + 1;
+   } else{
+        document.getElementById("caja39").style.height = "0px";
+        document.getElementById("caja39").style.paddingTop = "0px"; 
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function divLogin40(){ 
+   if(clic==1){
+    document.getElementById("caja40").style.paddingTop = "10px";
+    document.getElementById("caja40").style.height = "45px";
+   clic = clic + 1;
+   } else{
+        document.getElementById("caja40").style.height = "0px";
+        document.getElementById("caja40").style.paddingTop = "0px"; 
+    clic = 1;
+   }   
+}
+
+var clic = 1;
+function divLogin41(){ 
+   if(clic==1){
+    document.getElementById("caja41").style.paddingTop = "10px";
+    document.getElementById("caja41").style.height = "45px";
+   clic = clic + 1;
+   } else{
+        document.getElementById("caja41").style.height = "0px";
+        document.getElementById("caja41").style.paddingTop = "0px"; 
+    clic = 1;
+   }   
+}
